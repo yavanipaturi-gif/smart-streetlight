dabd04438b8d4b49cb9e9fd02c6bdb49e83e8dcb
yavanipaturi-gif <yavanipaturi@gmail.com>
2026-08-19 20:00:17 +0530

    Update smart_street_light.ino

--- smart_street_light.ino
+++ smart_street_light.ino
@@ -1,1 +1,20 @@
+int ldrpin = A0;
+int ledpin = 9;
+
+void setup() {
+  pinMode(ledpin, OUTPUT);
+  serial.begin(9600);
+}
+void loop(){
+  int lightValue = analogRead(ldrpin);
+
+  serial.println(lightValue);
+  if(lightValue < 500) {
+    digitalWiring(ledPin, HIGH);
+  }
+  else {
+     digitalWrite(ledPin, Low);
+  }
+   delay(500);
+}
 
